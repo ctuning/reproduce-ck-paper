8f0d41b346e5ea97add891d758a11a696fb422a3
Grigori Fursin <grigori.fursin@cTuning.org>
2015-04-03 16:55:21 +0200

    * Adding features of data sets * Adding support to reproduce this experiment

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # paper-ck-2015
-Shared artifacts for our CK paper 
-presented at CPC'15 (London, UK)
+Shared artifacts for our CK paper presented at CPC'15 (London, UK)
+
+# Installation using CK
+ck pull repo:paper-ck-2015
+ck pull repo:ck-env
+ck pull repo:ck-autotuning
+
+# Reproducing first experiment
+* From CMD: 
+> python reproduce1.py
+
+# From ipython notebook (check that CK is installed as python module): 
+> iptyhon notebook reproduce1.ipynb
---
+++ reproduce1.py
@@ -0,0 +1,27 @@
+#
+# Collective Knowledge (Experiment to check that filter optimizations 
+#  may depend on image features and hence require run-time adaptation)
+#
+# See CK LICENSE.txt for licensing details
+# See CK Copyright.txt for copyright details
+#
+# Developer: Grigori Fursin, Grigori.Fursin@cTuning.org, http://cTuning.org/lab/people/gfursin
+#
+
+import os
+
+print('')
+print('We found considerable execution times speedups')
+print('when using -O3 -fno-if-conversion')
+print('with older GCC < 4.7.x on different x86 processors')
+print('for homogeneous images using crowd-tuning.')
+
+print('')
+print('Interestingly, newer GCC do not show speed up,')
+print('and produce slower code than older GCC on such images.')
+
+print('')
+print('This motivates our continuous crowd-tuning approach')
+print('combined with public repository of optimization knowledge.')
+
+os.system('ck reproduce program.experiment.speedup @reproduce1.json')
