9900f0b9b767eeb21ddd8f2895b0838cd031b8a0
Grigori Fursin <Grigori.Fursin@cTuning.org>
2015-04-03 11:23:32 +0200

    removing tmp dir

--- program/shared-codelet-filter/tmp/tmp-flmjmh.bat
+++
@@ -1,5 +0,0 @@
-call D:\Work1\CK\ck-repos\fgg-local-win\env\a2aae29bf1e0257d\env.bat
-
-set CT_REPEAT_MAIN=1000
-
-a.exe D:\Work1\CK\ck-repos\paper-ck-2015\dataset\image-raw-bin-fgg-office-night-gray\data.bin > tmp-output1.tmp 2> tmp-output2.tmp
