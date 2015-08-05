53d7fd82ab72e1db999b896d1d30555446e3bf4d
Grigori Fursin <grigori.fursin@cTuning.org>
2015-08-05 16:33:00 +0200

    fixing reproducible example from our CM/CK paper

--- script/reproduce-filter-speedup/reproduce.py
+++ script/reproduce-filter-speedup/reproduce.py
@@ -12,10 +12,10 @@ import os
 import sys
 
 print('')
-print('We found considerable execution times speedups')
+print('We found considerable execution times speedup (~20%)')
 print('when using -O3 -fno-if-conversion')
 print('with older GCC < 4.7.x on different x86 processors')
-print('for homogeneous images using crowd-tuning.')
+print('for very dark images during crowd-tuning.')
 
 print('')
 print('Interestingly, newer GCC do not show speed up,')
@@ -23,7 +23,16 @@ print('and produce slower code than older GCC on such images.')
 
 print('')
 print('This motivates our continuous crowd-tuning approach')
-print('combined with public repository of optimization knowledge.')
+print('combined with public repository of optimization knowledge,')
+print('as described in http://arxiv.org/abs/1506.06256 .')
+
+print('')
+print('This script attemts to reproduce and validate our experiments,')
+print('and share results (crowdsourcing experiments).')
+
+print('')
+print('Note: if you run this script on Windows, add --target_os=mingw-64')
+print('      or --target_os=mingw-32 .')
 
 arg=sys.argv
 sarg=''
@@ -31,4 +40,4 @@ for q in sys.argv:
     if sarg=='': sarg=' ' # Pass first entry
     else: sarg+=' '+q
 
-os.system('ck reproduce program.experiment.speedup @reproduce.json '+sarg)
+os.system('ck reproduce program.experiment.speedup @reproduce.json --check_speedup '+sarg)
